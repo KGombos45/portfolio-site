21badb95028c06e77248c5f3ba28038e91bd776c
Kevin Gombos <gomboskevin@gmail.com>
2021-03-02 21:18:32 -0500

    add contact form email service and toast messsages

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,11 +2,46 @@ import React from 'react'
 import './contact.scss';
 import ScrollAnimation from 'react-animate-on-scroll';
 import { Button } from 'antd';
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function AppContact() {
+
+export default function AppContact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_86m9zqq', 'template_9smier7', e.target, 'user_riigRc7g9Tt3dpEWepHtq')
+          .then((result) => {
+              toast.success("Email sent succesfully! Thanks for reaching out!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+              e.target.reset();
+          }, (error) => {
+              toast.error("An error occured sending your email, feel free to contact me at: gomboskevin@gmail.com. Thank you!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                });
+          });
+        }
+
+
     return (
 
         <section className="contact" id="contact">
+        <ToastContainer />
         <div className="body-container flex">
             <ScrollAnimation animateIn='slideInLeft' initiallyVisible={false} animateOnce={true} duration={1}>
                 <div className="section-title">
@@ -22,13 +57,11 @@ function AppContact() {
                 </div>
             </ScrollAnimation>
             <ScrollAnimation animateIn='slideInUp' initiallyVisible={false} animateOnce={true} duration={1}>
-                <form className="contact-form">
-                    <input type="text" placeholder="Name" required></input>
-                    <input type="email" placeholder="Email" required></input>
-                    <textarea type="text" placeholder="Enter your message here..." required></textarea>
-                    <Button type="primary" className="submit-btn">
-                        Submit
-                    </Button>
+                <form className="contact-form" onSubmit={sendEmail}>
+                    <input type="text" placeholder="Name" name="name" required></input>
+                    <input type="email" placeholder="Email" name="email" required></input>
+                    <textarea type="text" placeholder="Enter your message here..." name="subject" required></textarea>
+                    <input type="submit" className="submit-btn" value="Submit"></input>
                 </form>
             </ScrollAnimation>
         </div>
@@ -36,4 +69,3 @@ function AppContact() {
     )
 }
 
-export default AppContact
