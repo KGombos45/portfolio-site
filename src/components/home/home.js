b5c6281f31d595961dcc6477e95cf6aeb96ace43
Kevin Gombos <gomboskevin@gmail.com>
2021-03-20 11:56:10 -0400

    style updates

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -17,7 +17,7 @@ function AppHome() {
                     params={{
                         "particles": {
                             "number": {
-                                "value": 40,
+                                "value": 60,
                                 "density": {
                                     "enable": true,
                                     "value_area": 800
