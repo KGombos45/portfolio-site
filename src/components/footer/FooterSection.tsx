81dc223cd3d1a9867e86c596b20d9de8549934f7
Kevin Gombos <gomboskevin@gmail.com>
2022-03-05 03:09:58 -0500

    check in

--- src/components/footer/FooterSection.tsx
+++ src/components/footer/FooterSection.tsx
@@ -1,12 +1,10 @@
 import {Box, Button, Typography} from '@material-ui/core';
+import {Link} from 'react-scroll';
 import {ExpandLess} from '../../assets';
+import {SectionTitles} from '../../base-components';
 import {useAppData, useAppStyles, useFooterStyles} from '../../hooks';
 
 export const FooterSection: React.FC = () => {
-  const scrollToTop = () => {
-    document.documentElement.scrollTo({top: 0, behavior: 'smooth'});
-  };
-
   const {flexContainerColumnCenter, flexContainerRowCenter} = useAppStyles();
   const {footer, scrollToTopButton, sitesButton, footnoteText, buttonIcon} = useFooterStyles();
   const {footerData} = useAppData();
@@ -14,9 +12,11 @@ export const FooterSection: React.FC = () => {
   return (
     <footer className={footer}>
       <Box className={flexContainerColumnCenter}>
-        <Button className={scrollToTopButton} onClick={() => scrollToTop()}>
-          <ExpandLess className={buttonIcon} />
-        </Button>
+        <Link smooth spy duration={1000} to={SectionTitles.Home}>
+          <Button className={scrollToTopButton}>
+            <ExpandLess className={buttonIcon} />
+          </Button>
+        </Link>
       </Box>
 
       <Box className={flexContainerRowCenter}>
